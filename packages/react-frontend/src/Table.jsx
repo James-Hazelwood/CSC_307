60c0bd1482b1e01f130c295dd324a34d696746f3
james <jameshazelwood023@gmail.com>
2025-04-29 10:50:23 -0700

    fix

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -3,6 +3,7 @@ function TableHeader() {
     return (
         <thead>
             <tr>
+                <th>Index</th>
                 <th>Name</th>
                 <th>Job</th>
                 <th>Delete??</th>
@@ -12,13 +13,22 @@ function TableHeader() {
 }
 
 function TableBody(props) {
+    console.log(props);
+    if (!Array.isArray(props.characterData) || props.characterData.length === 0){
+        return (
+        <tbody>
+        </tbody>
+        )
+    }
+
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
+                <td>{row._id}</td>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
                 <td>
-                    <button onClick={() => props.removeCharacter(index)}>
+                    <button onClick={() => props.removeCharacter(props.characterData[index])}>
                         Delete
                     </button>
                 </td>
